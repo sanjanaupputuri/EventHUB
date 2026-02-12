8eb20e89c6423f951420dc77a6b6feaa44adb15c
sanjanaupputuri <upputurisanjana@gmail.com>
2026-02-12 18:44:43 +0530

    Update PWA: fix start_url and enable auto-updates

--- sw.js
+++ sw.js
@@ -1,44 +1,25 @@
-const CACHE_NAME = 'unis-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/js/data.js',
-  '/manifest.json'
-];
+const CACHE_NAME = 'unis-v' + Date.now();
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request)
-        .then(fetchResponse => {
-          if (fetchResponse.ok) {
-            const clone = fetchResponse.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          }
-          return fetchResponse;
-        })
-      )
-      .catch(() => caches.match('/index.html'))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
